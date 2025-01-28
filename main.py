2559d223644e0a3771780c578d746535740baac1
aditya <meenakshi.bhtt@gmail.com>
2025-01-28 13:23:16 +0530

    started auth route

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from app.auth.router import router as auth_router
 
 app = FastAPI(title="Job-Dundo.AI")
 
 # Mount static and template directories
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+# Include the auth router
+app.include_router(auth_router)
 
 @app.get("/")
 async def home(request: Request):
